5ca8c21029a4cfd03f2adbb51c6408bf2bf4c10a
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-19 21:30:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using CodeBlogQueue.Model;
+using System;
+
+
+namespace CodeBlogQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyQueue = new EasyQueue<int>();
+            easyQueue.Enqueue(1);
+            easyQueue.Enqueue(2);
+            easyQueue.Enqueue(3);
+            easyQueue.Enqueue(4);
+            easyQueue.Enqueue(5);
+
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Peek());
+            Console.WriteLine(easyQueue.Dequeue());
+
+            Console.ReadLine();
+        }
+    }
+}
