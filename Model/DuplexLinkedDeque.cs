04cb262695a18ab5b5cefd3cdd9f5b2d7c0755fb
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-21 04:27:39 +0300

    Queue

---
+++ Model/DuplexLinkedDeque.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogQueue.Model
+{
+    public class DuplexLinkedDeque<T>
+    {
+        private DuplexItem<T> head;
+        private DuplexItem<T> tail; 
+        public int Count { get; set; }
+        public DuplexLinkedDeque() { }
+
+        public DuplexLinkedDeque(T data)
+        {
+            SetHeadItem(data);
+        }
+
+        private void SetHeadItem(T data)
+        {
+            var item = new DuplexItem<T>(data);
+            head = item;
+            tail = item;
+            Count = 1;
+        }
+
+        public void PushBack(T data)
+        {
+            if(Count == 0)
+            {
+                SetHeadItem(data);
+                return;
+            }
+
+            var item = new DuplexItem<T>(data);
+            item.Next = tail;
+            tail.Previous = item;
+            tail = item;
+            Count++;
+        }
+
+        public void PushFront(T data)
+        {
+            if (Count == 0)
+            {
+                SetHeadItem(data);
+                return;
+            }
+
+            var item = new DuplexItem<T>(data);
+            item.Previous = head;
+            tail.Next = item;
+            head = item;
+            Count++;
+        }
+
+        public T PopBack()
+        {
+            if(Count > 0)
+            {
+                var result = tail.Data;
+                var current = tail.Next;
+                current.Previous = null;
+                Count--;
+                return result;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public T PopFront()
+        {
+            if (Count > 0)
+            {
+                var result = head.Data;
+                var current = head.Previous;
+                current.Next = null;
+                Count--;
+                return result;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public T PeekBack()
+        {
+            return tail.Data;
+        }
+
+        public T PeekFront()
+        {
+            return head.Data;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,67 @@ namespace CodeBlogQueue
     {
         static void Main(string[] args)
         {
+            var duplexLinkedDeque = new DuplexLinkedDeque<int>();
+            duplexLinkedDeque.PushFront(1);
+            duplexLinkedDeque.PushFront(2);
+            duplexLinkedDeque.PushFront(3);// некоректно
+            duplexLinkedDeque.PushBack(4);
+            duplexLinkedDeque.PushBack(5);
+           
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+
+            Console.ReadLine();
+
+            var easyDeque = new EasyDeque<int>();
+            easyDeque.PushFront(1);
+            easyDeque.PushFront(2);
+            easyDeque.PushFront(3);
+            easyDeque.PushBack(40);
+            easyDeque.PushBack(50);
+            easyDeque.PushFront(600);
+            easyDeque.PushBack(700);
+
+            Console.WriteLine(easyDeque.PopFront());
+            Console.WriteLine(easyDeque.PopBack());
+            Console.WriteLine(easyDeque.PopFront());
+            Console.WriteLine(easyDeque.PopBack());
+            
+
+            Console.ReadLine();
+
+            var linkedQueue = new LinkedQueue<int>();
+            linkedQueue.Enqueue(1);
+            linkedQueue.Enqueue(2);
+            linkedQueue.Enqueue(3);
+            linkedQueue.Enqueue(4);
+            linkedQueue.Enqueue(5);
+
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Peek());
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Dequeue());
+
+            Console.ReadLine();
+
+
+            var arrayQueue = new ArrayQueue<int>(10);
+            arrayQueue.Enqueue(1);
+            arrayQueue.Enqueue(2);
+            arrayQueue.Enqueue(3);
+            arrayQueue.Enqueue(4);
+            arrayQueue.Enqueue(5);
+
+            Console.WriteLine(arrayQueue.Dequeue());
+            Console.WriteLine(arrayQueue.Peek());
+            Console.WriteLine(arrayQueue.Dequeue());
+
+            Console.ReadLine();
+
             var easyQueue = new EasyQueue<int>();
             easyQueue.Enqueue(1);
             easyQueue.Enqueue(2);
